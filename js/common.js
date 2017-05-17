1b796bc444409a8358323af215e2f2dc1a5b46d4
takawaki <nishiwaki.takafumi@gmail.com>
2017-05-17 12:11:42 +0000

    kadai3

--- js/common.js
+++ js/common.js
@@ -2,12 +2,13 @@ $(function(){
    
    //モーダルウィンドウの設定
    $("#menu_btn").click(function(){
-       //モーダルコンテンツの位置を変数で求める
-       var b_width = $(window).width();
-       var c_width = $("#modal_content").outerWidth();
-       var p_width = $(".close").outerWidth();
-       var position = ((b_width - c_width) / 2);
-       var position2 = ((b_width - p_width) / 2); 
+       
+       //モーダルコンテンツの位置を常に真ん中に位置するように設定
+       var b_width = $(window).width(); //ウィンドウ画面の幅を取得
+       var c_width = $("#modal_content").outerWidth(); //コンテンツの幅を取得
+       var p_width = $(".close").outerWidth(); //コンテンツの幅を取得
+       var position = ((b_width - c_width) / 2);　//水平中央にに位置するための余白を求める
+       var position2 = ((b_width - p_width) / 2); //水平中央にに位置するための余白を求める
        
         if($("#modal-overlay")[0]) return false ;	//新しくモーダルウィンドウを起動しない
         $("body").append("<div id='modal_overlay'>");
@@ -20,8 +21,9 @@ $(function(){
         
         $("#modal_overlay").click(function(){
             $("#modal_overlay,#modal_content").fadeOut();
-        })
-   })
+        })//click
+        
+   })//click
    
    //カルーセル設定
    $(".owl-carousel").owlCarousel(
@@ -35,38 +37,38 @@ $(function(){
              loop:true
           }
        }
-   });
+   });//owlcarousel
    
-   //商品画像のホバーアニメーション（TOPページのみ。他のページの画像はCSSで設定）
+   
+   //商品画像のホバーアニメーション
    $(".contents_detail > div").hover(function(){
        $(".item_img" ,this).animate({opacity:.5},300)
        },function(){
        $(".item_img" ,this).animate({opacity:1},300)
-       })
+       })//hover
   
   
    //product.htmlの商品詳細画像の切り替えアニメーション
    $(".thumb_list li img").click(function(){
        var switch_img = $(this).attr("src");
        
-       var switcher = function(){
-           $("#main_view").attr({src:switch_img});
-           $("#main_view").animate({opacity:"1"},500);
-          
-       }
-       
-       //もし、すでにクリックした画像#main_viewに表示されていなかったら切り替えアニメーションを実行
+       //もし、すでにクリックした画像が#main_viewに表示されていなかったら切り替えアニメーションを実行
         if($(this).attr("src") !== $("#main_view").attr("src")){
             
+            //画像切り替えアニメーションを定義
+               var switcher = function(){
+                   $("#main_view").attr({src:switch_img});
+                   $("#main_view").animate({opacity:"1"},500);
+               }
+               
+        //クリックしたら一度フェードアウトしてから画像を切り替えて、再びフェードイン(switcher関数を実行)
           $("#main_view").stop().animate({opacity:"0"},500,'linear',switcher);
           
         }//if
-    
    })//click
   
-   
-   
-   //サブコンテンツのリストアニメーション
+  
+   //サブコンテンツのリストスライドアニメーション
    $(".list_show_title").next("ul").hide();
    $(".list_show_title").click(function(){
       if($(this).hasClass('list_show_title')){
@@ -74,18 +76,16 @@ $(function(){
       }else if($(this).hasClass('down')){
          $(this).removeClass('down').addClass('list_show_title');
       }
-       //$(this).addClass('down');
+       
        $(this).next().stop().slideToggle();
-       
-       
+    
    });//click
   
+  
    //トップに戻るアニメーション
-   
    $("#page_top_btn img").click(function(){
-      
       $("body,html").animate({scrollTop:0},500);
       return false;
-       
-   })
-})
+   })//click
+   
+})//all
